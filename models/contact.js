b3049767cd1f52cea1d9e325f64066cf976e734a
priyanshurajput <priyanshu@hypergro.ai>
2025-07-06 14:18:53 +0530

    model created and db connection

---
+++ models/contact.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const Contact = sequelize.define('Contact', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  phoneNumber: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  linkedId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  linkPrecedence: {
+    type: DataTypes.ENUM('primary', 'secondary'),
+    allowNull: false,
+    defaultValue: 'primary',
+  },
+}, {
+  timestamps: true,
+  tableName: 'contacts',
+});
+
+module.exports = Contact;
