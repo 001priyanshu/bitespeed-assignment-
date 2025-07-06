f96876bf940b34ee1a84827f1e215f3c10e58304
priyanshurajput <priyanshu@hypergro.ai>
2025-07-06 14:17:36 +0530

    base setup

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { sequelize } = require('./config/database');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK' });
+});
+
+
+
+async function start() {
+  try {
+    await sequelize.authenticate();
+    console.log('✅ DB connected');
+    await sequelize.sync();
+    console.log('✅ Models synced');
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('❌ Error connecting to DB:', error);
+  }
+}
+
+start();
